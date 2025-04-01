ff4f9a8c5f9ffddffce7ea5219533b8371cae598
Vladimir Novikov <vladimir.novikov.k@yandex.ru>
2025-04-01 17:56:40 +0300

    Safe concept for parser input iterator.

--- projects/lib/kaleidoscope/include/kaleidoscope/parser.hpp
+++ projects/lib/kaleidoscope/include/kaleidoscope/parser.hpp
@@ -2,13 +2,30 @@
 
 #include <kaleidoscope/ast.hpp>
 
+namespace kaleidoscope::concepts
+{
+
+template<typename T>
+inline constexpr bool is_safe_trivially_copyable_v =
+        std::is_trivially_copyable_v<T>
+        && !std::is_polymorphic_v<T> // Блокирует virtual-методы
+        && !std::is_pointer_v<T>;    // Блокирует сырые указатели
+
+template<typename Iterator>
+concept TriviallyCopyableIterator =
+        std::contiguous_iterator<Iterator>
+        && is_safe_trivially_copyable_v<
+                std::remove_cvref_t<
+                        typename std::iterator_traits<Iterator>::value_type>>;
+} // namespace kaleidoscope::concepts
+
 namespace kaleidoscope
 {
 
 struct Parser
 {
-    template<typename Iterator>
-    static Ast parse(Iterator begin, Iterator end);
+    template<concepts::TriviallyCopyableIterator T>
+    static Ast parse(T begin, T end);
 };
 
 } // namespace kaleidoscope
